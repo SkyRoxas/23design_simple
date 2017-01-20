b5dcdc7eb861b09d8fae3e9d3b0f292f8106c974
skyroxas <skyroxas1521@gmail.com>
2017-01-20 16:45:07 +0800

    hidden

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,9 @@ $.fn.rotate_menu = function($settings) {
         $('.rotate-body').toggleClass('rotate_back');
         $('.rotate-menu').toggleClass('rotate_front');
         setTimeout(function(){
+          $('.rotate-body').toggleClass('rotate-menu-hidden');
           $('.rotate-menu').toggleClass('rotate-menu-index');
+
         },1000);
     })
     if ($settings.navHtml !== null) {
